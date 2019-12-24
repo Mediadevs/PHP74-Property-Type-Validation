9dd6917d8692294df2307295f16525afc8e42e53
mikevandiepen <mike.vandiepen@hotmail.com>
2019-12-24 20:04:56 +0100

    Worked on the docblock analysis trait

--- src/Analyser/Traits/AnalyseDocblock.php
+++ src/Analyser/Traits/AnalyseDocblock.php
@@ -2,6 +2,8 @@
 
 namespace Mediadevs\StrictlyPHP\Analyser\Traits;
 
+use phpDocumentor\Reflection\DocBlock;
+
 /**
  * Trait AnalyseDocblock.
  *
@@ -9,5 +11,68 @@ namespace Mediadevs\StrictlyPHP\Analyser\Traits;
  */
 trait AnalyseDocblock
 {
+    /**
+     * Collecting the return from the docblock.
+     * If "Null" is returned it means there is NO return type, if a string is returned there is a return type.
+     *
+     * @param \phpDocumentor\Reflection\DocBlock $docBlock
+     *
+     * @return string|null
+     */
+    protected function getReturnTypeFromDocblock(DocBlock $docBlock): ?string
+    {
+        return null;
+    }
 
+    /**
+     * Collecting the parameter from the docblock based upon the given parameter.
+     * If "Null" is returned it means there is NO parameter type, if a string is returned there is a parameter type.
+     *
+     * @param \phpDocumentor\Reflection\DocBlock $docBlock
+     * @param string                             $parameter
+     *
+     * @return string|null
+     */
+    protected function getParameterTypeFromDocblock(DocBlock $docBlock, string $parameter): ?string
+    {
+        return null;
+    }
+
+    /**
+     * Collecting the property type from the docblock.
+     * If "Null" is returned it means there is NO property type, if a string is returned there is a property type.
+     *
+     * @param \phpDocumentor\Reflection\DocBlock $docBlock
+     * @param string                             $parameter
+     *
+     * @return string|null
+     */
+    protected function getPropertyTypeFromDocblock(DocBlock $docBlock, string $parameter): ?string
+    {
+        return null;
+    }
+
+    /**
+     * Validating whether the docblock is suppressed by "inheritdoc" (Parent class docblock).
+     *
+     * @param \phpDocumentor\Reflection\DocBlock $docBlock
+     *
+     * @return bool
+     */
+    private function isSuppressedByInheritDoc(DocBlock $docBlock): bool
+    {
+        return false;
+    }
+
+    /**
+     * Validating whether the type is suppressed by a compound type for example "string|null".
+     *
+     * @param \phpDocumentor\Reflection\DocBlock $docBlock
+     *
+     * @return bool
+     */
+    private function isSuppressedByCompoundType(DocBlock $docBlock): bool
+    {
+        return false;
+    }
 }
